79bcdb88a047d1a2b6507c0e33540c90465b4e9f
Anus <david.korotainen@gmail.com>
2026-01-16 12:30:02 +0200

    moodle2

---
+++ moodle2/6.py
@@ -0,0 +1,13 @@
+import random
+
+a=random.randint(0,9)
+b=random.randint (0,9)
+c=random.randint(0,9)
+
+d=random.randint(1,6)
+e=random.randint(1,6)
+x=random.randint(1,6)
+g=random.randint(1,6)
+
+print(f"3-digit code: {a}{b}{c}")
+print(f"4-digit code: {d}{e}{x}{g}")
