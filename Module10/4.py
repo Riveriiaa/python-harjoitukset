d2a1a597260f985cf538caa13b3b230449ad2f37
Anus <david.korotainen@gmail.com>
2026-03-24 02:38:24 +0200

    Moodle 10

---
+++ Module10/4.py
@@ -0,0 +1,44 @@
+import random
+
+
+class Car:
+    def __init__(self, license_plate, maximum_speed):
+        self.license_plate = license_plate
+        self.maximum_speed = maximum_speed
+        self.current_speed = 0
+        self.travelled_distance = 0
+
+    def accelerate(self, speed_change):
+        self.current_speed += speed_change
+
+        if self.current_speed > self.maximum_speed:
+            self.current_speed = self.maximum_speed
+        if self.current_speed < 0:
+            self.current_speed = 0
+
+    def drive(self, hours):
+        self.travelled_distance += self.current_speed * hours
+
+
+class Race:
+    def __init__(self, name, distance, cars):
+        self.name = name
+        self.distance = distance
+        self.cars = cars
+
+    def hour_passes(self):
+        for car in self.cars:
+            speed_change = random.randint(-10, 15)
+            car.accelerate(speed_change)
+            car.drive(1)
+
+    def print_status(self):
+        print(f"{'License plate':<15}{'Max speed':<15}{'Current speed':<18}{'Travelled distance':<20}")
+        for car in self.cars:
+            print(f"{car.license_plate:<15}{car.maximum_speed:<15}{car.current_speed:<18}{car.travelled_distance:<20.1f}")
+
+    def race_finished(self):
+        for car in self.cars:
+            if car.travelled_distance >= self.distance:
+                return True
+        return False
